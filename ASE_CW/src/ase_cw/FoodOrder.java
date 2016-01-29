5f6a447e3756cbdf46540abfe43d6f7385339fff
Cécile Riquart <cecile.riquart@eisti.fr>
2016-01-29 11:59:06 +0000

    Adding the FoodOrder class + test first push

---
+++ ASE_CW/src/ase_cw/FoodOrder.java
@@ -0,0 +1,11 @@
+package ase_cw;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eisti
+ * Date: 1/29/16
+ * Time: 11:58 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FoodOrder {
+}
